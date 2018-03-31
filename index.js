fa0ec95b2418928f9b680588cd49dbda1edb24fe
mnap <learn@10g.pl>
2018-03-31 14:04:47 +0200

    Add read/write scripts

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ var StatMode = require('stat-mode');
 fs.stat('./cat.jpg', function(err, stats) {
     var statMode = new StatMode(stats);
     console.log(statMode.toString());
+    console.log('\n');
+});
+
+fs.readFile('./text.txt', 'utf-8', function(err, data) {
+    console.log(data);
+});
+
+fs.writeFile('./text.txt', 'Tekst, który zapiszemy w pliku\n', function(err) {
+    if (err) throw err;
+    console.log('Saved!');
 });
