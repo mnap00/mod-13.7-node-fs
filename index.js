df79edc582b34c9e531e7b983bb2d1530b2227bf
mnap <learn@10g.pl>
2018-03-31 14:45:17 +0200

    Add direcotry listing

--- index.js
+++ index.js
@@ -11,12 +11,22 @@ fs.stat('./cat.jpg', function(err, stats) {
 fs.readFile('./text.txt', 'utf-8', function(err, data) {
     console.log('Data before write:'.blue);
     console.log(data);
-    fs.writeFile('./text.txt', 'This text will be saved\n', function(err) {
+    fs.appendFile('./text.txt', '\nThis text will be saved', function(err) {
         if (err) throw err;
         console.log('Saved!'.blue);
         fs.readFile('./text.txt', 'utf-8', function(err, data) {
             console.log('Saved data:'.blue);
             console.log(data);
         });
+    });
+});
+
+fs.readdir('./', function (err, files) {
+    var dir = '';
+    for (var i = 0; i < files.length; i++) {
+        dir += files[i] + '\n';
+    }
+    fs.writeFile('./dir.txt', dir, function(err) {
+        if (err) throw err;
     });
 });
